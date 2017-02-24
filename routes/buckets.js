0e004efb8967b36cf7da2d82f0c7d7fb74f1b26c
Jonathan Mancia <jonathanman2010@icloud.com>
2017-02-23 19:34:47 -0700

    buckets routes

---
+++ routes/buckets.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const Bucket = require('../models/bucket');
+const List = require('../models/list');
+const Card = require('../models/card');
+
+
+router.get('/', (req, res) => {
+    Bucket.find( ( err, data ) => {
+        res.json(data);
+    });
+});
+
+router.post('/', (req, res) =>{
+    new Bucket({
+        name: req.body.name
+    }).save( (err, data) => {
+        res.json(data);
+    });
+});
